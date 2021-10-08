ec55a7a6ae54f1e7872013e3061a5e5c2779b116
Syafiqjos <syafiqjos@gmail.com>
2021-10-08 12:32:05 +0700

    Add crud template to links api route

--- src/RestApi/v1/routes/links.js
+++ src/RestApi/v1/routes/links.js
@@ -2,4 +2,25 @@ module.exports = function(app){
     app.get('/api/links', (req, res) => {
         res.send('ok bg.');
     });
+
+    app.get('/api/links/:id', (req, res) => {
+        res.send('ok bg.' + req.params.id);
+    });
+
+    app.post('/api/links', (req, res) => {
+        res.send('ok bg.');
+    });
+    
+    app.patch('/api/links/:id', (req, res) => {
+        res.send('ok bg.');
+    });
+
+    app.put('/api/links/:id', (req, res) => {
+        res.send('ok bg.');
+    });
+
+    app.delete('/api/links/:id', (req, res) => {
+        res.send('ok bg.');
+    });
+    
 }
