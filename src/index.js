17efab2a0ae1f4946cdac8eec05a2d3269fa56c3
Syafiqjos <syafiqjos@gmail.com>
2021-10-08 20:39:36 +0700

    Install mongoose and setting up database

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
+// EXPRESS
 const app = express();
+
+// DOTENV
 dotenv.config();
+
+// DATABASE
+const dbUri = process.env.DATABASE_URI;
+mongoose.connect(dbUri).catch(err => { console.log(err); });
 
 // REST API
 const RestApi = require('./RestApi');
