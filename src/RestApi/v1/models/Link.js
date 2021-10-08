8b59aefe0274464737291638ae046f20139e452d
Syafiqjos <syafiqjos@gmail.com>
2021-10-08 20:59:59 +0700

    Add Database Schema and Model - Link

---
+++ src/RestApi/v1/models/Link.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const linkSchema = mongoose.Schema({
+    redirect_url: {
+        type: String,
+        required: true
+    },
+    custom_url: {
+        type: String,
+        required: true
+    },
+    author: {
+        // type: String // Semisal gaada author berarti gabisa dihapus
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    created_at: {
+        type: Date
+    },
+    updated_at: {
+        type: Date
+    }
+});
+
+const Link = mongoose.model('Link', linkSchema);
+
+module.exports = Link;
