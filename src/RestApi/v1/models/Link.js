eb830c9e59945da31644cdb7bdad9d5cdc5c6578
Syafiqjos <syafiqjos@gmail.com>
2021-10-10 16:50:50 +0700

    Update Link schema to be able work with User

--- src/RestApi/v1/models/Link.js
+++ src/RestApi/v1/models/Link.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const linkSchema = mongoose.Schema({
     redirect_url: {
         type: String,
-        required: true
+        required: [true, 'Insert redirect_url field!']
     },
     custom_url: {
         type: String,
         required: true,
-        unique: true
+        required: [true, 'Insert custom_url field!']
     },
     author: {
         // type: String // Semisal gaada author berarti gabisa dihapus
@@ -22,6 +22,32 @@ const linkSchema = mongoose.Schema({
         updatedAt: 'updated_at'
     }
 });
+
+linkSchema.post('save', async function(err, doc, next) {
+    if (err.name === 'MongoServerError' && err.code === 11000) {
+        next(Error('custom-link already exists! Choose other else!'));
+    } else {
+        next(err);
+    }
+});
+
+linkSchema.post('updateOne', async function(err, doc, next) {
+    if (err.name === 'MongoServerError' && err.code === 11000) {
+        next(Error('custom-link already exists! Choose other else!'));
+    } else {
+        next(err);
+    }
+});
+
+linkSchema.statics.truncateData = function(link) {
+    if (link) {
+        return {
+            redirect_url: link.redirect_url,
+            custom_url: link.custom_url,
+        }
+    }
+    return null;
+}
 
 const Link = mongoose.model('Link', linkSchema);
 
