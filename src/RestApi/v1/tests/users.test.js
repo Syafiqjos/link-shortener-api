db54a7911e0046e4c272bc2dd478e98a5ff23dcb
Syafiqjos <syafiqjos@gmail.com>
2021-10-16 18:15:52 +0700

    Add jest Login test on users route

--- src/RestApi/v1/tests/users.test.js
+++ src/RestApi/v1/tests/users.test.js
@@ -72,3 +72,87 @@ test('Error on Register Existed User', async () => {
 
     expect(response.status).toEqual('error');
 });
+
+test('Error on Login User to get jwt token without provide email and password and fullname', async () => {
+    const options = {
+        hostname: 'localhost',
+        port: 5000,
+        path: '/api/v1/user/login',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    const response = JSON.parse(await httpRequest(options));
+
+    expect(response.status).toEqual('error');
+}); 
+
+test('Error on Login User to get jwt token with specific field only (email, password)', async () => {
+    const options = {
+        hostname: 'localhost',
+        port: 5000,
+        path: '/api/v1/user/login',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    expect(JSON.parse(await httpRequest(options, { email })).status).toEqual('error');
+    expect(JSON.parse(await httpRequest(options),{ password }).status).toEqual('error');
+}); 
+
+test('Error on Login Unregistered user', async () => {
+    const options = {
+        hostname: 'localhost',
+        port: 5000,
+        path: '/api/v1/user/login',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    const response = JSON.parse(await httpRequest(options, { email: 'kadal@gmail.com', password: 'somepass' }));
+
+    token = response.data;
+
+    expect(response.status).toEqual('error');
+});
+
+test('Error on Login Registered user with invalid pass', async () => {
+    const options = {
+        hostname: 'localhost',
+        port: 5000,
+        path: '/api/v1/user/login',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    const response = JSON.parse(await httpRequest(options, { email, password: 'invalidpass' }));
+
+    expect(response.status).toEqual('error');
+});
+
+test('Login Registered user to get jwt token', async () => {
+    const options = {
+        hostname: 'localhost',
+        port: 5000,
+        path: '/api/v1/user/login',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    const response = JSON.parse(await httpRequest(options, { email, password }));
+
+    token = response.data;
+    console.log(token);
+
+    expect(response.status).toEqual('success');
+});
